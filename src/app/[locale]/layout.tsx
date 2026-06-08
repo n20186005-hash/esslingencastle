f9a094ca719d3f1d7d3aae0779a360c1282dab51
n20186005-hash <n20186005@gmail.com>
2026-06-08 15:45:09 +0800

    更新

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -14,7 +14,7 @@ export async function generateMetadata({
   params: Promise<{ locale: string }>;
 }): Promise<Metadata> {
   const { locale } = await params;
-  const messages = (await import(`@/messages/${locale}.json`)).default;
+  const messages = (await import(`@/messages/${locale}.js`)).default;
   const baseUrl = 'https://jardimluisdecamoes.com';
 
   const zhUrl = `${baseUrl}/zh`;
